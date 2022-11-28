8df893ab04b4f39786c936ce4e2c3cc3e2bc4c53
Goksu Okar <goksuokar@gmail.com>
2022-11-28 10:35:25 -1000

    styling

--- src/routes/NewOrder.jsx
+++ src/routes/NewOrder.jsx
@@ -189,153 +189,156 @@ export default function NewOrder() {
 
 
   return (
-    <Container>
-      <h1>NEW ORDER FORM</h1>
-        <Stepper active={active} breapoint='sm' onStepClick={(val) => changeToActive(val)}>
-          <Stepper.Step description='Intro'>
-            <Radio.Group
-              name='intro'
-              defaultValue={['walk in']}
-              label='INTRO'
-              withAsterisk
-              {...form.getInputProps('intro')}
-            >
-              <Radio size='sm' value='walk in' label='walk in'/>
-              <Radio size='sm' value='phone' label='phone'/>
-              <Radio size='sm' value='email' label='email'/>
-              <Radio size='sm' value='website' label='website'/>
-            </Radio.Group>
-            <Group>
+    <div>
+      <a href={`/`}>Home</a>
+      <Container>
+          <h1>New Order</h1>
+          <Stepper active={active} breapoint='sm' onStepClick={(val) => changeToActive(val)}>
+            <Stepper.Step description='Intro'>
+              <Radio.Group
+                name='intro'
+                defaultValue={['walk in']}
+                label='INTRO'
+                withAsterisk
+                {...form.getInputProps('intro')}
+              >
+                <Radio size='sm' value='walk in' label='walk in'/>
+                <Radio size='sm' value='phone' label='phone'/>
+                <Radio size='sm' value='email' label='email'/>
+                <Radio size='sm' value='website' label='website'/>
+              </Radio.Group>
+              <Group>
+                <TextInput
+                  withAsterisk
+                  label="First Name"
+                  {...form.getInputProps('firstName')}
+                />
+                <TextInput
+                  withAsterisk
+                  label="Last Name"
+                  {...form.getInputProps('lastName')}
+                />
+              </Group>
+              <Space h="md"/>
+              <Checkbox
+                label="This is a new customer"
+                {...form.getInputProps('current', {type: 'Checkbox'})}
+              />
+              <Radio.Group
+                name='order type'
+                defaultValue={['walk in']}
+                label='Order Type'
+                withAsterisk
+                onChange={() => console.log('change')}
+                {...form.getInputProps('orderType')}
+              >
+                <Radio size='sm' value='Surf' label='Surf'/>
+                <Radio size='sm' value='Windsurf' label='Windsurf'/>
+                <Radio size='sm' value='Foil' label='Foil'/>
+              </Radio.Group>
+              <Radio.Group
+                name='customer type'
+                defaultValue={['retail']}
+                label='Customer Type'
+                withAsterisk
+                {...form.getInputProps('customerType')}
+              >
+                <Radio size='sm' value='retail' label='Retail'/>
+                <Radio size='sm' value='team' label='Team'/>
+                <Radio size='sm' value='gratis' label='Gratis'/>
+                <Radio size='sm' value='wholesale' label='Wholesale'/>
+              </Radio.Group>
+
+              <Select
+                label="Approved By (only for team and gratis orders)"
+                allowDeselect
+                placeholder="Approved by"
+                data={[
+                  { value: 'lalo', label: 'Lalo' },
+                  { value: 'francisco', label: 'Francisco' },
+                  { value: 'pascal', label: 'Pascal' },
+                  { value: 'keith', label: 'Keith' },
+                ]}
+                {...form.getInputProps('approvedBy')}
+              />
+            </Stepper.Step>
+
+            <Stepper.Step description='contact info'>
               <TextInput
                 withAsterisk
-                label="First Name"
-                {...form.getInputProps('firstName')}
+                label="Customer Email"
+                placeholder="customer@email.com"
+                {...form.getInputProps('email')}
               />
               <TextInput
-                withAsterisk
-                label="Last Name"
-                {...form.getInputProps('lastName')}
+                label="Phone Number"
+                {...form.getInputProps('phone')}
               />
-            </Group>
-            <Space h="md"/>
-            <Checkbox
-              label="This is a new customer"
-              {...form.getInputProps('current', {type: 'Checkbox'})}
-            />
-            <Radio.Group
-              name='order type'
-              defaultValue={['walk in']}
-              label='Order Type'
-              withAsterisk
-              onChange={() => console.log('change')}
-              {...form.getInputProps('orderType')}
-            >
-              <Radio size='sm' value='Surf' label='Surf'/>
-              <Radio size='sm' value='Windsurf' label='Windsurf'/>
-              <Radio size='sm' value='Foil' label='Foil'/>
-            </Radio.Group>
-            <Radio.Group
-              name='customer type'
-              defaultValue={['retail']}
-              label='Customer Type'
-              withAsterisk
-              {...form.getInputProps('customerType')}
-            >
-              <Radio size='sm' value='retail' label='Retail'/>
-              <Radio size='sm' value='team' label='Team'/>
-              <Radio size='sm' value='gratis' label='Gratis'/>
-              <Radio size='sm' value='wholesale' label='Wholesale'/>
-            </Radio.Group>
+              <Textarea
+                placeholder='customer address'
+                label='Address'
+                {...form.getInputProps('address')}
+              />
+            </Stepper.Step>
 
-            <Select
-              label="Approved By (only for team and gratis orders)"
-              allowDeselect
-              placeholder="Approved by"
-              data={[
-                { value: 'lalo', label: 'Lalo' },
-                { value: 'francisco', label: 'Francisco' },
-                { value: 'pascal', label: 'Pascal' },
-                { value: 'keith', label: 'Keith' },
-              ]}
-              {...form.getInputProps('approvedBy')}
-            />
-          </Stepper.Step>
-
-          <Stepper.Step description='contact info'>
-            <TextInput
-              withAsterisk
-              label="Customer Email"
-              placeholder="customer@email.com"
-              {...form.getInputProps('email')}
-            />
-            <TextInput
-              label="Phone Number"
-              {...form.getInputProps('phone')}
-            />
-            <Textarea
-              placeholder='customer address'
-              label='Address'
-              {...form.getInputProps('address')}
-            />
-          </Stepper.Step>
-
-          <Stepper.Step description="rider info">
-            <NumberInput
-              label="Weight:"
-              hideControls
-              placeholder="weight in lb"
-              {...form.getInputProps('weight')}
-            />
-            <Group>
+            <Stepper.Step description="rider info">
               <NumberInput
-                label="Ft:"
-                placeholder="ft"
-                {...form.getInputProps('heightFt')}
+                label="Weight:"
+                hideControls
+                placeholder="weight in lb"
+                {...form.getInputProps('weight')}
               />
-              <NumberInput
-                label="Inch:"
-                placeholder="in"
-                {...form.getInputProps('heightIn')}
+              <Group>
+                <NumberInput
+                  label="Ft:"
+                  placeholder="ft"
+                  {...form.getInputProps('heightFt')}
+                />
+                <NumberInput
+                  label="Inch:"
+                  placeholder="in"
+                  {...form.getInputProps('heightIn')}
+                />
+              </Group>
+              <Select
+                label="Level"
+                allowDeselect
+                placeholder="pick one"
+                data={[
+                  { value: 'entry', label: 'entry' },
+                  { value: 'intermediate', label: 'intermediate' },
+                  { value: 'advanced', label: 'advanced' },
+                  { value: 'pro', label: 'pro' },
+                ]}
+                {...form.getInputProps('level')}
               />
-            </Group>
-            <Select
-              label="Level"
-              allowDeselect
-              placeholder="pick one"
-              data={[
-                { value: 'entry', label: 'entry' },
-                { value: 'intermediate', label: 'intermediate' },
-                { value: 'advanced', label: 'advanced' },
-                { value: 'pro', label: 'pro' },
-              ]}
-              {...form.getInputProps('level')}
-            />
-          </Stepper.Step>
+            </Stepper.Step>
 
-          <Stepper.Step description="Board Specs">
+            <Stepper.Step description="Board Specs">
+              {boardType === "Surf" ? (
+                <SurfSpecs form={form}/>
+              ) : boardType === "Windsurf" ? (<WindsurfSpecs form={form}/>) : (<FoilSpecs form={form}/>)}
+            </Stepper.Step>
+
+          <Stepper.Completed>
             {boardType === "Surf" ? (
-              <SurfSpecs form={form}/>
-            ) : boardType === "Windsurf" ? (<WindsurfSpecs form={form}/>) : (<FoilSpecs form={form}/>)}
-          </Stepper.Step>
-
-        <Stepper.Completed>
-          {boardType === "Surf" ? (
-              <PdfSurf form={form}/>
-            ) : boardType === "Windsurf" ?
-            (<PdfWS form={form}/>) : (<PdfFoil form={form}/>)
-          }
-        </Stepper.Completed>
-        </Stepper>
-        <Group position="right" mt="xl">
-          {active !== 0 && (
-            <Button variant="default" onClick={prevStep}>
-              Back
-            </Button>
-          )}
-          {active < 3 && <Button onClick={nextStep}>Next step</Button>}
-          {active === 3 && <Button onClick={nextStep}>Finish Order</Button>}
-          {active > 3 && <Button onClick={handleGeneratePdf}>Generate Pdf</Button>}
-        </Group>
-    </Container>
+                <PdfSurf form={form}/>
+              ) : boardType === "Windsurf" ?
+              (<PdfWS form={form}/>) : (<PdfFoil form={form}/>)
+            }
+          </Stepper.Completed>
+          </Stepper>
+          <Group position="right" mt="xl">
+            {active !== 0 && (
+              <Button variant="default" onClick={prevStep}>
+                Back
+              </Button>
+            )}
+            {active < 3 && <Button onClick={nextStep}>Next step</Button>}
+            {active === 3 && <Button onClick={nextStep}>Finish Order</Button>}
+            {active > 3 && <Button onClick={handleGeneratePdf}>Generate Pdf</Button>}
+          </Group>
+      </Container>
+    </div>
   )
 };
--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,9 +1,13 @@
+import { Group } from '@mantine/core';
+
 export default function Home() {
   return (
     <div>
       <h1>Forward Maui</h1>
-      <div>search bar</div>
-      <a href={`new-order`}>New order</a>
+      <Group>
+        <div>search bar</div>
+        <a href={`new-order`}>New order</a>
+      </Group>
     </div>
   )
 }
