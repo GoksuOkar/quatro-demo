8899474d42057d295b7478e892a9f638bc892f90
Goksu Okar <goksuokar@gmail.com>
2022-12-14 09:07:39 -1000

    resolve home

--- src/main.jsx
+++ src/main.jsx
@@ -6,19 +6,6 @@ const loginloader = function () {
   return true;
 }
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/login",
-//     element: <Login />,
-//     loader: loginloader,
-
-//   },
-//   {
-//     path: "/",
-//     element: <Home />,
-//   }
-// ]);
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
