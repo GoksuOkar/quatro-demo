4cc68cd15674ad1c05934740fc6a68031607bb06
Goksu Okar <goksuokar@gmail.com>
2023-01-25 15:59:47 -1000

    search works with keypress

--- src/routes/Orders.jsx
+++ src/routes/Orders.jsx
@@ -1,25 +1,26 @@
 import * as React from 'react';
 import { useState, useEffect, useRef } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
-import { Table, Container, Alert, Pagination, TextInput, Button, Group } from '@mantine/core';
+import { Table, Container, Alert, Pagination, TextInput, Button, Group, Center } from '@mantine/core';
 import axios from 'axios';
 import { capitalizeFirstLetter } from "../utils/helpers.js";
 import { Axios } from '../utils/helpers.js';
 
+const convertDate = (d) => {
+  let date = new Date(d);
+  return date.toISOString().slice(0, 10);
+};
+
 export default function Orders() {
-  // const location = useLocation();
-  // const navigate = useNavigate();
+  const location = useLocation();
+  const navigate = useNavigate();
   const [activePage, setPage] = useState(1);
   const [orders, setOrders] = useState([]);
   const searchRef = useRef(null);
 
 
-  const convertDate = (d) => {
-    let date = new Date(d);
-    return date.toISOString().slice(0, 10);
-  };
 
-  const handleOrderClick = (o) => {
+  const goToOrder = (o) => {
     Axios.get(`/customers/id/${o.customerId.valueOf()}`)
       .then((res) => {
           navigate('/order', {state: {customer: res.data, order: o}})
@@ -27,7 +28,8 @@ export default function Orders() {
       );
   };
 
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
     let input = searchRef.current.value.toLowerCase();
     if (input === "surf" || input === "windsurf" || input === "foil") {
       Axios.get(`/orders/${input}`).then((res) => {
@@ -53,7 +55,7 @@ export default function Orders() {
   }
 
   const rows = orders.map((o) => (
-    <tr key={o.orderId} onClick={() => {handleOrderClick(o)}}>
+    <tr key={o.orderId} onClick={() => {goToOrder(o)}}>
       <td>{convertDate(o.date)}</td>
       <td>{capitalizeFirstLetter(o.customerName)}</td>
       <td>{capitalizeFirstLetter(o.orderType)}</td>
@@ -63,12 +65,15 @@ export default function Orders() {
 
   return (
     <Container>
-      <Container>
-        <Group>
-          <TextInput placeholder="search order" ref={searchRef}/>
-          <Button color="dark" onClick={handleSubmit}>submit</Button>
-        </Group>
-      </Container>
+      <a href={`/`}>Home</a>
+      <Center>
+        <form onSubmit={handleSubmit}>
+          <Group>
+            <TextInput placeholder="search order" ref={searchRef}/>
+            <Button color="dark" type="submit">submit</Button>
+          </Group>
+        </form>
+      </Center>
       <Table highlightOnHover>
         <thead>
           <tr>
@@ -80,7 +85,7 @@ export default function Orders() {
         </thead>
         <tbody>{rows}</tbody>
       </Table>
-      <Pagination page={activePage} onChange={setPage} total={5}/>
+      {orders.length > 20 ? <Pagination page={activePage} onChange={setPage} total={orders.length / 20}/> : null}
     </Container>
   )
 }
