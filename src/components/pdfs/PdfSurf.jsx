ebdbb07506f74f38b1859f8508155b26e8d25e6c
imperium11 <poyraztcc@gmail.com>
2023-10-17 14:13:02 -1000

    Working on new printout design

--- src/components/pdfs/PdfSurf.jsx
+++ src/components/pdfs/PdfSurf.jsx
@@ -1,7 +1,8 @@
-import { Button, Title } from '@mantine/core';
+import { Button, Title, Avatar } from '@mantine/core';
 import { useRef, useEffect, useState } from 'react';
 import './output.css';
 import { capitalizeFirstLetter, convertDate } from "../../utils/helpers.js";
+import myAvatar from '../../pictures/black-avatar.jpg'
 
 //surf output
 
@@ -12,178 +13,74 @@ export default function PdfSurf({ values, orderNum, customer }) {
 
   return (
     <>
-    <div className="printme" id="printcontents">
-      <div className="child">
-        <p>
-          <b>Name:</b>
-          {` ${name}`}
-        </p>
-        {customer.phone === "" ? null : (
-          <>
-            <b>Phone:</b>
-            <p style={{ marginTop: 0 }}>{customer.phone}</p>
-          </>
-        )}
-        {customer.address === "na" ? null : (
-          <>
-            <b>Address:</b>
-            <p style={{ marginTop: 0 }}>{customer.address}</p>
-          </>
-        )}
-        {customer.email === "" ? null : (
-          <>
-            <b>Email:</b>
-            <p style={{ marginTop: 0 }}>{customer.email}</p>
-          </>
-        )}
-      </div>
-      {/* RIDER INFO */}
-      <div className="child">
-        <p>
-          <b>Weight:</b>
-          {` ${customer.weight}lbs`}
-        </p>
-        <p>
-          <b>Height:</b>
-          {` ${customer.heightFt}ft ${customer.heightIn}in`}
-        </p>
-        <p>
-          <b>Level:</b>
-          {` ${customer.level}`}
-        </p>
-        <p>
-          <b>Location:</b>
-          {` ${values.waveLocation}`}
-        </p>
-      </div>
-      <div className="child">
-        <p className="orderNum">
-          <b>Order Number: {` ${orderNum}`}</b>
-        </p>
-        <p>
-          <b>Date:</b>
-          {` ${convertDate(values.createdAt)}`}
-        </p>
-        <p>
-          <b>Order Type:</b>
-          {` ${values.orderType}`}
-        </p>
-        <p>
-          <b>Invoice Number:</b>
-          {` ${values.invoiceNum}`}
-        </p>
-        <p>
-          <b>Intro:</b>
-          {` ${values.intro}`}
-        </p>
-      </div>
-      {/* BOARD SPECS */}
-      <div className="child">
-        <p>
-          <b>Style:</b>
-          {` ${values.style}`}
-        </p>
-        <p>
-          <b>Construction:</b>
-          {` ${values.construction}`}
-        </p>
-        <p>
-          <b>Length:</b>
-          {` ${values.lengthFt}ft ${values.lengthIn}`}
-        </p>
-        <p>
-          <b>Box:</b>
-          {` ${values.boxType}`}
-        </p>
-        <p>
-          <b>Inserts:</b>
-          {` ${values.inserts}`}
-        </p>
-        <p>
-          <b>Board Color:</b>
-          {` ${values.boardColor}`}
-        </p>
-        <p>
-          <b>Box Color:</b>
-          {` ${values.boxColor}`}
-        </p>
-      </div>
+      <div className='outer-box'>
 
-      <div className="child">
-        <p>
-          <b>Tail:</b>
-          {` ${values.tail}`}
-        </p>
-        <p>
-          <b>Width:</b>
-          {` ${values.width}`}
-        </p>
-        <p>
-          <b>Fin Setup:</b>
-          {` ${values.finSetup}`}
-        </p>
-        <p>
-          <b>Stance:</b>
-          {` ${values.stance}`}
-        </p>
-        <p>
-          <b>Logo:</b>
-          {` ${values.logo}`}
-        </p>
-        <p>
-          <b>Pads:</b>
-          {` ${values.pads}`}
-        </p>
-      </div>
+        <div className='title-box'>
+          <div className='order-type-heading'>
+            <div className='order-title'>{capitalizeFirstLetter(values.orderType)}</div>
+          </div>
+          <div className='order-date'>
+            <span>Order Created</span>
+            {convertDate(values.createdAt)}
+          </div>
+        </div>
 
-      <div className="child">
-        <p>
-          <b>Tow Weight:</b>
-          {` ${values.towWeight}`}
-        </p>
-        <p>
-          <b>Blank:</b>
-          {` ${values.blank}`}
-        </p>
-        <p>
-          <b>Thickness:</b>
-          {` ${values.thickness}`}
-        </p>
-        <p>
-          <b>Straps Width:</b>
-          {` ${values.strapWidth}`}
-        </p>
-        <p>
-          <b>Volume:</b>
-          {` ${values.volume}`}
-        </p>
-        <p>
-          <b>Rear Strap From Tail:</b>
-          {` ${values.rearStrap}`}
-        </p>
-        <p>
-          <b>Leash Plug:</b>
-          {` ${values.leash}`}
-        </p>
-        <p>
-          <b>Airbrush:</b>
-          {` ${values.airbrush}`}
-        </p>
-        <p>
-          <b>Board Weight:</b>
-          {` ${values.boardWeight}`}
-        </p>
-      </div>
-      <div className="child" id="notes">
-        <b>Notes:</b>
-        <p style={{marginTop: '5px'}}>{values.notes}</p>
-      </div>
-      <div className="child">
-        <img id="image" src="/surfBoard.jpeg" />
-      </div>
-    </div>
-      <div className='rush'>
-        {values.dueDate === "" ? null : (<div className='r-div'>DUE {values.dueDate}</div>)}
+        <div className='contact-box'>
+          <div className='avatar-info'>
+            <Avatar src={myAvatar} size='80px'></Avatar>
+            <span>Customer Information</span>
+          </div>
+          <div className='c-info-in-box'>
+            {/* One */}
+            <div className='contact-info'>
+              <div className='one-data'>
+                <span>Name </span>
+                {name}
+              </div>
+              <div className='one-data'>
+                <span>Phone </span>
+                {customer.phone === "na" ? <div>&nbsp;</div> : customer.phone}
+              </div>
+              <div className='one-data'>
+                <span>Email </span>
+                {customer.email === "na" ? null: customer.email}
+              </div>
+            </div>
+            {/* Two */}
+            <div className='contact-info'>
+              <div className='one-data'>
+                <span>Weight </span>
+                {customer.weight}
+              </div>
+              <div className='one-data'>
+                <span>Height </span>
+                {` ${customer.heightFt}' ${customer.heightIn}`}
+              </div>
+              <div className='one-data'>
+                <span>Level </span>
+                {capitalizeFirstLetter(customer.level)}
+              </div>
+            </div>
+
+            {/* Three */}
+            <div className='contact-info'>
+              <div className='one-data'>
+                <span>Location </span>
+                {capitalizeFirstLetter(values.waveLocation)}
+              </div>
+              <div className='one-data'>
+                <span>Order #</span>
+                {orderNum}
+              </div>
+              <div className='one-data'>
+                <span>Invoice #</span>
+                {values.invoiceNum === '' ? <div>&nbsp;</div> : values.invoiceNum}
+              </div>
+            </div>
+
+          </div>
+        </div>
+          
       </div>
     </>
   );
