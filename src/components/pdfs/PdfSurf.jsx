7a3cc9e3788546aa7a0f356085928f1d9c885851
Poyraz Akay <poyraztcc@gmail.com>
2023-11-29 14:18:29 -1000

    Qr code (#31)
    * rendering QR Code
    
    * created qr-code for every order

--- src/components/pdfs/PdfSurf.jsx
+++ src/components/pdfs/PdfSurf.jsx
@@ -1,16 +1,22 @@
-import { Avatar } from '@mantine/core';
-import { useRef, useEffect, useState } from 'react';
+import { Avatar,  } from '@mantine/core';
+import { useEffect, useState } from 'react';
 import './output.css';
 import { capitalizeFirstLetter, convertDate, createTodaysDate, futureDate6 } from "../../utils/helpers.js";
 import myAvatarPerson from '../../pictures/black-avatar.jpg'
 import myAvatarBoard from '../../pictures/board-black.png'
 import myAvatarPen from '../../pictures/black-pen1.png'
+import QRCode from 'qrcode';
 //surf output
 
 export default function PdfSurf({ values, orderNum, customer }) {
 
-  // console.log('surfValues:', values)
-  const myref = useRef(null);
+  const [src, setSrc] = useState('');
+
+  const url = 'https://forms.gle/CQz4sQkKEXN6q4on6';
+
+  useEffect(() => {
+    QRCode.toDataURL(url).then(setSrc);
+  }, []);
 
   let name = capitalizeFirstLetter(customer.firstName + " " + customer.lastName);
 
@@ -22,16 +28,21 @@ export default function PdfSurf({ values, orderNum, customer }) {
           <div className='order-type-heading'>
             <div className='order-title'>{capitalizeFirstLetter(values.orderType)}</div>
           </div>
-          {values.createdAt ? (
-            <div className='order-date'>
-              <span>Order Created</span>
-              {convertDate(values.createdAt)}
-          </div>) : (
-            <div className='order-date'>
-              <span>Order Created</span>
-              {createTodaysDate()}
-          </div> 
-          )}
+          <div className='right-box-top'>
+            {values.createdAt ? (
+              <div className='order-date'>
+                <span>Order Created</span>
+                {convertDate(values.createdAt)}
+            </div>) : (
+              <div className='order-date'>
+                <span>Order Created</span>
+                {createTodaysDate()}
+            </div> 
+            )}
+            <div>
+              <img className='qr-code' src={src}/>
+            </div>
+          </div>
         </div>
 
         <div className='contact-box'>
